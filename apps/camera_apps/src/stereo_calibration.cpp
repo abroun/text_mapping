68d372771218c423e40d4b0ac47e413fef162c54
Chris <cjbeck.meng@gmail.com>
2012-09-13 15:08:29 +0100

    Changed camera_calibration with new subpixel varibles Start working on stereo_calibration

--- apps/camera_apps/src/stereo_calibration.cpp
+++ apps/camera_apps/src/stereo_calibration.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+
+
 int main(int argc, char** argv)
 {
 
